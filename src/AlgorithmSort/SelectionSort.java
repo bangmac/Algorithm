d0eb530ac4a3126b9e98c4dcc4efd02fb3e86291
bmv <leeloo1899@gmail.com>
2020-01-16 13:34:52 +0700

    update

--- src/AlgorithmSort/SelectionSort.java
+++ src/AlgorithmSort/SelectionSort.java
@@ -5,20 +5,19 @@ public class SelectionSort {
     public static void selectionSort(double[] list) {
         for (int i = 0; i < list.length - 1; i++) {
             /* Find the minimum in the list[i..list.length-1] */
-            double currentMin = list[i];
-            int currentMinIndex = i;
+            double currentValueMin = list[i]; // gan gia tri min bang phan tu thu
+            int currentIndexMin = i;
 
             for (int j = i + 1; j < list.length; j++) {
-                if (currentMin > list[j]) {
-                    currentMin = list[j];
-                    currentMinIndex = j;
+                if (currentValueMin > list[j]) {
+                    currentValueMin = list[j];
+                    currentIndexMin = j;
                 }
             }
-
-            /* Swap list[i] with list[currentMinIndex] if necessary */
-            if (currentMinIndex != i) {
-                list[currentMinIndex] = list[i];
-                list[i] = currentMin;
+                /* Swap list[i] with list[currentIndexMin] if necessary */
+                if (currentIndexMin != i) {
+                list[currentIndexMin] = list[i];
+                list[i] = currentValueMin;
             }
         }
     }
